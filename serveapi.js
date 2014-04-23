e5dcdc594bc6dfa3bc612337578e8ef72d90c607
Satish Talluri <satish@neptune.io>
2014-04-23 01:30:47 -0700

    API server working with CORS settings on safari only

--- serveapi.js
+++ serveapi.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var noConnections =0;
 var objToJson = {
-  Name:"John Art",
-  Email: "john.art@johnart.com",
-  Address:"Tenclave drive, #304,Minneapolis, MT, 94305",
-  Role:"Adminstrator"
+  userDetailsName:"John,Art",
+  userDetailsEmail:"john.art@johnart.com",
+  userDetailsRole:"NormalEngineer",
+  userDetailsNewsletter:"No",
+  userDetailsKeys:"adlkjfladjfd2343acxcclkjldafdfajldf"
 };
+
 var objToJson2 = {
   Job:"John Art",
   Email: "john.art@johnart.com",
@@ -36,11 +38,22 @@ var objToJson3= {
   }};
 
   http.createServer(function(req, res){
-    //you will realize with logging that there will be two requests when you click in a browser. Probably one for favicon
+
+    //Set required access controll headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control');
+    
+    console.log(req.url);
+    console.log(req.url.replace(/\?_=\d+/g,""));
+    console.log("replace url params"+req.url);
+
     //Base on requested URL serve the JSON
-    switch(req.url) {
+    switch(req.url.replace(/\?_=\d+/g,"")) {
       case '/person' :
         console.log("LOG : Request received for person : "+Date.now());
+      // res.setHeader('Access-Control-Allow-Origin','http://localhost:8888/person');
       res.writeHead(200, {'Content-Type':'application/json'});
       res.end(JSON.stringify(objToJson));
       console.log("LOG : Response served successfully at : "+Date.now());
@@ -64,6 +77,6 @@ var objToJson3= {
       console.log("LOG : Response served successfully at : "+Date.now());
       break;
     }
-  }).listen(8888,'127.0.0.1');
+  }).listen(8080,'127.0.0.1');
 
   console.log('First node API webserver just started!');
