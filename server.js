fe75ca8a105c51aceb52986263e5160be665ebdb
Satish Talluri <satish@neptune.io>
2014-05-23 12:46:55 -0700

    server changes

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-var http = require('http');
+var fs=require('fs');
+//Using http only instead of https for lack of SSL keys
+var https = require('https');
 var noConnections =0;
+var selfCert={
+  key:fs.readFileSync('/Users/satish/.ssh/stalluri_self_ssl/key.pem'),
+  cert:fs.readFileSync('/Users/satish/.ssh/stalluri_self_ssl/cert.pem')
+}
 
-http.createServer(function(req, res){
+//Data to be sent in response to a http request
+var jsonToSend = {
+  key1 : "value1",
+  key2: "value2",
+  key3: "value3"
+}
+var imgToSend=fs.readFileSync('./apple.png');
+var textToSend="Saying hello from a node server";
+
+//Start the server on port 8888
+https.createServer(selfCert,function(req, res){
         //you will realize with logging that there will be two requests when you click in a browser. Probably one for favicon
         console.log("LOG : Request received at : "+Date.now());
-        res.writeHead(200, {'Content-Type':'text/plain'});
-        res.end('First node webserver saying hello!!');
+
+        //Send a json
+        // res.writeHead(200, {'Content-Type':'application/json'});
+        // res.end(JSON.stringify(jsonToSend));
+
+        //Send an image
+        // res.writeHead(200, {'Content-Type':'img/png'});
+        // res.end(imgToSend,'binary');
+
+        //Send a plain text
+         res.writeHead(200, {'Content-Type':'text/plain'});
+         res.end(textToSend);
+
         console.log("LOG : Response served successfully at : "+Date.now());
-        console.log('LOG : No of connections :'+ ++noConnections);
+        console.log('LOG : No of connections served so far :'+ ++noConnections);
 }).listen(8888,'127.0.0.1');
 
 console.log('First node webserver just started!');
--- serverTest.js
+++ serverTest.js
@@ -1,15 +1,20 @@
-//Concurrent connections server test
+//Script to test a simle nodejs server
+//Before running this script run ulimit -n 8192 on your local 
+//machine to increase number of file descriptors without which you will run into errors 
 
 var http = require('http');
-var noOfConcurrentConnections =1000000;
+
+//Testing variables here
+//Set the maximum no of concurrent connections
+var maxConcurrentConnections =1000000;
 
 // Agent default to max sockets of 5, we need more
-http.globalAgent.maxSockets = noOfConcurrentConnections ;
+http.globalAgent.maxSockets = maxConcurrentConnections ;
 
 
-var requestOptions = { 
+var requestOptions = {
   host: '127.0.0.1',
-  port:8080,
+  port:8888,
   path:'/',
   agent:false,
   method:'GET'
@@ -17,29 +22,42 @@ var requestOptions = {
 
 //Response handler
 var responseHandler = function (res) {
-
-console.log("Request successfull");  
+  var sentData='';
+  console.log("Request successfull");
+  res.resume();
+  res.on('data', function (chunk) {
+    if(res.statusCode == 200){
+      console.log("Request successfull");
+      sentData += chunk;
+    }
+  });
+  res.on('end',function(){
+    console.log("Client connected :" +i);
+    console.log(sentData);
+  }).on('error', function(e) {
+    console.log("Error: " + options.host + "\n" + e.message);
+  });;
 
 }
 
-// create more, agents un till we have enough 
-for(var i = 0; i<=4000; i++){
+// create more, agents until we have enough
+for(var i = 0; i<=4; i++){
 
   http.request(requestOptions,responseHandler).end();
- 
- /* function (res) {
-    res.resume();
-    res.on('data', function (chunk) {
-            if(res.statusCode == 200){
-                console.log("Request successfull");            }
-        });
-    res.on('end',function(){
-      console.log("Client connected :" +i);
-    }).on('error', function(e) {
-       console.log("Error: " + options.host + "\n" + e.message);
-    });;
-  
-  
-  });*/
+
+  /* function (res) {
+     res.resume();
+     res.on('data', function (chunk) {
+     if(res.statusCode == 200){
+     console.log("Request successfull");            }
+     });
+     res.on('end',function(){
+     console.log("Client connected :" +i);
+     }).on('error', function(e) {
+     console.log("Error: " + options.host + "\n" + e.message);
+     });;
+
+
+     });*/
 
 }
